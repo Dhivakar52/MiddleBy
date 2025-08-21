ab6ec5a5b047ce8b194196d09d205c3cfad2bd33
Dhivakar52 <awsstg@thefederal.in>
2025-08-21 12:44:43 +0530

    Leadership message script

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,4 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
   tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl);
   });
+
+
+
+
+const messages = document.querySelectorAll('.message_expand');
+const toggleBtns = document.querySelectorAll('.message_show');
+
+toggleBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    messages[index].classList.toggle("expanded");
+
+    if (messages[index].classList.contains("expanded")) {
+      btn.innerHTML = `Read Less <i class="fa fa-chevron-up"></i>`;
+    } else {
+      btn.innerHTML = `Read Full Message <i class="fa fa-chevron-down"></i>`;
+    }
+  });
+});
+
+
+
+
+
+
 });
